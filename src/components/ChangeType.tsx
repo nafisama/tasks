eb83fc4318a8c8544773ac678635fac4040960d7
Maryam <nafisama@udel.edu>
2022-10-25 18:09:21 -0400

    Task-8 ,& image issue from Task 3

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -1,7 +1,37 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
+//type QuestionType = "multiple_choice_question" | "short_answer_question";
 
+// Maps the Old state -> New State
+const Transitions: Record<QuestionType, QuestionType> = {
+    // eslint-disable-next-line prettier/prettier
+    "multiple_choice_question": "short_answer_question",
+    // eslint-disable-next-line prettier/prettier
+    "short_answer_question": "multiple_choice_question"
+};
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [questionType, setQuestionType] = useState<QuestionType>(
+        "short_answer_question"
+    );
+    function changeQuestionT(): void {
+        const newQuestion = Transitions[questionType];
+        setQuestionType(newQuestion);
+    }
+
+    return (
+        <div>
+            <div>
+                Current Question Type: <span>{questionType}</span>
+                <Button onClick={changeQuestionT}>Change Type</Button>
+            </div>
+            <div>
+                {questionType === "multiple_choice_question" ? (
+                    <span>Multiple Choice</span>
+                ) : (
+                    <span>Short Answer</span>
+                )}
+            </div>
+        </div>
+    );
 }
--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,50 @@
+/* eslint-disable prettier/prettier */
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    type Holidays =
+        | "Ramadan"
+        | "NewYears"
+        | "Christmas"
+        | "Father's Day"
+        | "Mother's Day";
+    const Alpha: Record<Holidays, Holidays> = {
+        // eslint-disable-next-line prettier/prettier
+        "Christmas": "Father's Day",
+        "Father's Day": "NewYears",
+        // eslint-disable-next-line prettier/prettier
+        "NewYears": "Mother's Day",
+        "Mother's Day": "Ramadan",
+        // eslint-disable-next-line prettier/prettier
+        "Ramadan": "Christmas"
+    };
+    const YearC: Record<Holidays, Holidays> = {
+        // eslint-disable-next-line prettier/prettier
+        "NewYears": "Ramadan",
+        // eslint-disable-next-line prettier/prettier
+        "Ramadan": "Mother's Day",
+        // eslint-disable-next-line prettier/prettier
+        "Mother's Day": "Father's Day",
+        "Father's Day": "Christmas",
+        // eslint-disable-next-line prettier/prettier
+        "Christmas": "NewYears"
+    };
+    const [holiday, setHoliday] = useState<Holidays>("Ramadan");
+    function changeAlpha(): void {
+        const newH = Alpha[holiday];
+        setHoliday(newH);
+    }
+    function changeYear(): void {
+        const newH = YearC[holiday];
+        setHoliday(newH);
+    }
+
+    return <div>
+        <Button onClick={changeAlpha}>Alphabet</Button>
+        <Button onClick={changeYear}>Year</Button>
+        {// eslint-disable-next-line prettier/prettier, indent
+        holiday === "Ramadan" ? <span>Holiday: ☪️️</span>:holiday === "NewYears"?<span>Holiday: 🎄</span>:holiday === "Mother's Day"?<span>Holiday: 👩</span>:holiday === "Father's Day"?<span>Holiday: 👨</span>:<span>Holiday: 🎁</span>
+        }
+    </div>;
 }
