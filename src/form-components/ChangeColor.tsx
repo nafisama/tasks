e629735725dc78238aabfe9e3d22aa3a01605315
Maryam <nafisama@udel.edu>
2022-10-27 18:17:31 -0400

    Done :)

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,51 @@
 import React, { useState } from "react";
-
+import { Form } from "react-bootstrap";
+const COLORS = [
+    "red",
+    "blue",
+    "green",
+    "orange",
+    "purple",
+    "yellow",
+    "cyan",
+    "black"
+];
+const DEFAULT_COLOR = COLORS[0];
 export function ChangeColor(): JSX.Element {
+    const [favorite, setFavorite] = useState<string>(DEFAULT_COLOR);
+
     return (
         <div>
             <h3>Change Color</h3>
+            {COLORS.map((color: string) => (
+                <Form.Check
+                    key={color}
+                    inline
+                    type="radio"
+                    name="colors"
+                    onChange={(e: {
+                        target: { value: React.SetStateAction<string> };
+                    }) => setFavorite(e.target.value)}
+                    id={color}
+                    label={color}
+                    value={color}
+                    checked={favorite === color}
+                />
+            ))}
+            <div
+                data-testid="colored-box"
+                style={{
+                    width: "50px",
+                    height: "50px",
+                    backgroundColor: favorite,
+                    border: "1px solid blue",
+                    display: "inline-block",
+                    verticalAlign: "bottom",
+                    marginLeft: "5px"
+                }}
+            >
+                The color is {favorite}
+            </div>
         </div>
     );
 }
--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,30 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 export function GiveAttempts(): JSX.Element {
+    const [left, setLeft] = useState<number>(3);
+    const [request, setRequest] = useState<string>("");
+    const req_int = parseInt(request) || 0;
+    function updateRequest(event: React.ChangeEvent<HTMLInputElement>) {
+        setRequest(event.target.value);
+    }
+
     return (
         <div>
-            <h3>Give Attempts</h3>
+            Attempts Left = {left}
+            <Form.Group controlId="formUpdateRequest">
+                <Form.Label>Attempt Request:</Form.Label>
+                <Form.Control value={request} onChange={updateRequest} />
+                <Form.Control
+                    type="number"
+                    value={request}
+                    onChange={updateRequest}
+                />
+            </Form.Group>
+            <Button onClick={() => setLeft(left - 1)} disabled={left <= 0}>
+                use
+            </Button>
+            <Button onClick={() => setLeft(left + req_int)}>gain</Button>
         </div>
     );
 }
